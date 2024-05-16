d182deec7e188ee9d79c2710ffd406ab4051a7e7
wertyaser <nacmanleonielgarin@gmail.com>
2024-05-16 09:01:30 +0800

    modified:   src/store/homeStore.js

--- src/store/homeStore.js
+++ src/store/homeStore.js
@@ -1,7 +1,13 @@
 import { create } from "zustand";
 
 const homeStore = create((set) => ({
-  fetchCoins: () => {},
+  fetchCoins: () => {
+    fetch("https://api.coingecko.com/api/v3/coins/list")
+      .then((res) => res.json())
+      .then((data) => {
+        set({ coins: data });
+      });
+  },
 }));
 
 export default homeStore;
